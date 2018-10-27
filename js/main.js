aa81a8a7c41f4b3a0fa781eac97d2f44f2fbba02
oljuanfe <ojuanferrero@gmail.com>
2018-10-27 23:48:31 +0200

    Make navigation menu for mobil, add some JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-console.log('Ready :)')
+console.log('Ready :)');
+
+const buttonOpenMenu = document.querySelector('.nav-trigger');
+const navigationMenu = document.querySelector('.nav');
+const buttonCloseMenu = document.querySelector('.nav-close');
+
+buttonOpenMenu.innerHTML = '';
+buttonCloseMenu.innerHTML = '';
+
+navigationMenu.classList.add('hidden');
+
+console.log(buttonOpenMenu.style);
+buttonOpenMenu.style.backgroundImage = "url('../images/ico-menu.svg')";
+buttonCloseMenu.style.backgroundImage = "url('../images/ico-close.svg')";
+
+
+
+function handleOpenMenu() {
+  console.log('clickando');
+  navigationMenu.classList.toggle('hidden');
+}
+
+function handleCloseMenu() {
+  console.log('estoy clickando');
+  navigationMenu.classList.toggle('hidden');
+}
+
+buttonOpenMenu.addEventListener('click', handleOpenMenu);
+buttonCloseMenu.addEventListener('click', handleCloseMenu);
